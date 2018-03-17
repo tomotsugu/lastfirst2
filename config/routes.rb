487dda8352a7f731b26436a4c6fc5d6ad096f04f
tom <salem55.tk@gmail.com>
2018-03-17 05:08:32 +0000

    post-size

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'sessions/new'
-
+  root to: "top#index"
   resources :sessions, only: [:new, :create, :destroy]
   resources :posts
   resources :contacts do
