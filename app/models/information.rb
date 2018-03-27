97bac322d556dd4bc7b2788c32755790e815a9f4
tom <salem55.tk@gmail.com>
2018-03-27 13:41:44 +0000

    “pointing3”

--- app/models/information.rb
+++ app/models/information.rb
@@ -2,6 +2,6 @@ class Information < ApplicationRecord
   validates :title, presence: true, length: { maximum: 30 }
   validates :content, presence: true, length: { in: 1..200 }
   validates :destination, presence: true, length: { maximum: 200 }
-  validates :release, presence: true, length: { in: 1..1 }
-  validates_exclusion_of :release, in: 0..1, message: 'out of range 0 or 1'
+  validates :release, presence: true, length: { in: 1..1 }, :numericality => { :less_than_or_equal_to => 1 } 
+
 end
