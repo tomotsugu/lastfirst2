654772643e93baabd674c64549d140950fc9b9d9
tom <salem55.tk@gmail.com>
2018-03-27 12:50:37 +0000

    “pointing2”

--- app/models/information.rb
+++ app/models/information.rb
@@ -1,7 +1,7 @@
 class Information < ApplicationRecord
-  validates :title,  presence: true, length: { maximum: 30 }
-  validates :content,  presence: true, length: { in: 1..200 }
-  validates :destination,  presence: true, length: { maximum: 200 }
-  validates :release,  presence: true, length: { in: 1..1 }
-
+  validates :title, presence: true, length: { maximum: 30 }
+  validates :content, presence: true, length: { in: 1..200 }
+  validates :destination, presence: true, length: { maximum: 200 }
+  validates :release, presence: true, length: { in: 1..1 }
+  validates_exclusion_of :release, in: 0..1, message: 'out of range 0 or 1'
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   validates :content,  presence: true, length: { in: 1..400 }
   validates :release,  presence: true, length: { in: 1..1 }
+  validates_exclusion_of :release, in: 0..1, message: 'out of range 0 or 1'
 
   belongs_to :user
     
